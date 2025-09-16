83e4146ce53dc65463e34e6993d0457e70b30c81
Cauany Nunes Rodrigues <cauanyrodrigues01@gmail.com>
2025-09-16 17:57:12 -0300

    feat: adiciona script de inicialização para a página de contato com formulário e funcionalidades de menu

---
+++ assets/js/contact.js
@@ -0,0 +1,29 @@
+import { SmartForm } from "./modules/smartForm.js";
+import { initializeMobileMenu } from "./modules/menuMobile.js";
+import { smoothScroll } from "./modules/smoothScroll.js";
+import { initializeDropdow } from "./modules/dropdown.js";
+
+// Inicializa as funções da página de sobre nós
+document.addEventListener("DOMContentLoaded", () => {
+  // Inicializa o menu mobile
+  initializeMobileMenu();
+
+  // Inicializa os dropdowns
+  initializeDropdow();
+
+  // Inicializa o smooth scroll para âncoras
+  smoothScroll();
+
+  // INICIALIZAÇÃO DO FORMULÁRIO DE CONTATO
+  document.addEventListener("DOMContentLoaded", () => {
+    const contactForm = new SmartForm(
+      "contactForm",
+
+      async (data) => {
+        console.log("Dados do formulário de contato:", data);
+        // atraso artificial para simular a requisição de rede
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      }
+    );
+  });
+});
